40a83521cdf93a4ee2290daca1a3738d15af4580
P-Lisaveta <elizapotekhina@yandex.com>
2026-02-03 12:15:14 +0300

    NoGood

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"hexlet-path-size/PathSize"
 	"log"
 	"os"
 
@@ -14,6 +15,16 @@ func main() {
 	cmd := &cli.Command{
 		Name:  "hexlet-path-size",
 		Usage: "print size of a file or directory",
+		Action: func(c *cli.Context) error {
+			if c.Args().Len() == 0 {
+				return fmt.Errorf("path required")
+			}
+
+			path := c.Args().Get(0)
+			size := PathSize.GetSize(path)
+			fmt.Printf("%d\t%s\n", size, path)
+			return nil
+		},
 	}
 
 	if err := cmd.Run(context.Background(), os.Args); err != nil {
